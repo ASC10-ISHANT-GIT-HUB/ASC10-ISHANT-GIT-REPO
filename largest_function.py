b074accc55f48214debb93279067d5bce848b876
ASC10-ISHANT-GIT-HUB <bansalishant05@gmail.com>
2025-10-08 17:54:03 +0530

    Delete largest_function.py

--- largest_function.py
+++
@@ -1,8 +0,0 @@
-def find_maximum(num1, num2, num3):
-    return max(num1, num2, num3)
-
-a = float(input("Enter first number: "))
-b = float(input("Enter second number: "))
-c = float(input("Enter third number: "))
-
-print(f"The biggest number is: {find_maximum(a, b, c)}")
